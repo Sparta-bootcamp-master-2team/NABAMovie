136459008fc3fea9c76da4915a17e3a0c08cb3bb
minjae <qlqlzbto@naver.com>
2025-04-28 23:55:06 +0900

    feat: 마이 페이지 컬렉션 뷰 생성

--- NABAMovie/NABAMovie/Presentation/MyPageView/MyPageCollectionView.swift
+++ NABAMovie/NABAMovie/Presentation/MyPageView/MyPageCollectionView.swift
@@ -6,15 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
-class MyPageCollectionView: UICollectionView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class MyPageCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 125)
+        layout.minimumInteritemSpacing = 10
+        layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 50)
+        super.init(frame: frame, collectionViewLayout: layout)
+        self.backgroundColor = .white
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
