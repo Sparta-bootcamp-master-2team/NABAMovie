7d6488a441703dda40e8ab5d118e905a07381960
gnoes-ios <park_juseong@naver.com>
2025-04-28 13:25:08 +0900

    feat: Constants 파일 생성 및 .xcconfig를 사용하여 API키 숨기기

---
+++ NABAMovie/NABAMovie/Utils/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  NABAMovie
+//
+//  Created by 박주성 on 4/28/25.
+//
+
+
+import Foundation
+
+enum TMDB {
+    static let baseURL = "https://api.themoviedb.org/3"
+    static let apiKey = Bundle.main.object(forInfoDictionaryKey: "TMDB_API_KEY") as! String
+    static let language = "ko-KR"
+    static let posterBaseURL = "https://image.tmdb.org/t/p/w500"
+    
+    enum NowPlaying {
+        static let path = "/movie/now_playing"
+    }
+    
+    enum UpComing {
+        static let path = "/movie/upcoming"
+    }
+    
+    enum Search {
+        static let path = "/search/movie"
+    }
+    
+    enum Detail {
+        static let path = "/movie/"
+        static let appendToResponse = "credits,release_dates"
+    }
+}
