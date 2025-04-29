d180316c8e7fcc07d0afd3ca737be004cb78ff3d
EllGHT <rmsgh1188@gmail.com>
2025-04-29 14:04:10 +0900

    refactor: SceneDelegate 원상태로

--- NABAMovie/NABAMovie/App/SceneDelegate.swift
+++ NABAMovie/NABAMovie/App/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let viewController = MovieInfoViewController()
+        let viewController = MainViewController()
         self.window = UIWindow(windowScene: windowScene)
         window?.rootViewController = viewController
         
