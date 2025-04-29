0331d5ee8bca4847917ef4c2ee43f18c7238c05e
EllGHT <rmsgh1188@gmail.com>
2025-04-29 14:05:26 +0900

    feat: MovieEntity에서 데이터 로드

--- NABAMovie/NABAMovie/Presentation/MovieInfo/MovieInfoViewModel.swift
+++ NABAMovie/NABAMovie/Presentation/MovieInfo/MovieInfoViewModel.swift
@@ -11,13 +11,55 @@ import Kingfisher
 
 class MovieInfoViewModel {
     
+    let movieDetail: MovieEntity
+    
     var stillImages: [URL] = []
+    
+    var isFavorite: Bool = false
     
     private let usecase = FetchMovieStillsUseCase(repository: MovieRepositoryImpl(networkManager: MovieNetworkManager()))
     
+    init(movieDetail: MovieEntity) {
+        self.movieDetail = movieDetail
+    }
+    
+    var titleText: String {
+        movieDetail.title
+    }
+    
+    var posterURL: URL? {
+        guard let posterURLString = movieDetail.posterImageURL else { return nil }
+        return URL(string: posterURLString)
+    }
+    
+    var infoText: String {
+        let runtime = "\(Int(movieDetail.runtime) / 60)시간 \(Int(movieDetail.runtime) % 60)분"
+        return "\(movieDetail.releaseDate!) · \(runtime) · \(movieDetail.genre.joined(separator: ", "))"
+    }
+    
+    var voteAverageText: String {
+        String(format: "%.1f", movieDetail.voteAverage)
+    }
+    
+    var certificationText: String {
+        movieDetail.certification ?? "정보 없음"
+    }
+    
+    var directorText: String {
+        movieDetail.director ?? "정보 없음"
+    }
+    
+    var castText: String {
+        movieDetail.actors.joined(separator: ", ")
+    }
+    
+    var overviewText: String {
+        movieDetail.overview ?? "정보 없음"
+    }
+    
     func setStillImages(completion: @escaping () -> Void) {
         Task {
-            let result = await usecase.execute(for: 1197306)
+            let result = await usecase.execute(for: movieDetail.movieID)
             switch result {
             case .success(let movies):
                 print(movies.count)
@@ -30,16 +72,4 @@ class MovieInfoViewModel {
             }
         }
     }
-    
-    // MOCK DATA
-    let movieImage = UIImage(named: "mockPoster")
-    let title = "어벤져스: 인피니티 워"
-    let info = "2022﹒2시간 30분﹒액션, SF"
-    let rating = 4.8
-    let age = 12
-    let director = "Anthony Russo, Joe Russo"
-    let cast = "Robert Downey Jr., Chris Hemsworth, Chris Evans"
-    let description = "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos."
-    var isFavorite: Bool = false
-    
 }
