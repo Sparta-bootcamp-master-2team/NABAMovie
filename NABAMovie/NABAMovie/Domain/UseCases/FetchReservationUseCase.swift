0ec0947d464adb963b0cd05dcc6b9c2458db5fa3
_GGDol <bohg6799@naver.com>
2025-04-29 13:50:00 +0900

    feat: FetchReservationsUseCase 생성 (예매 내역 조회 기능)

--- NABAMovie/NABAMovie/Domain/UseCases/FetchReservationUseCase.swift
+++ NABAMovie/NABAMovie/Domain/UseCases/FetchReservationUseCase.swift
@@ -1,7 +1,25 @@
 //
-//  FetchReservation.swift
+//  FetchReservationUseCase.swift
 //  NABAMovie
 //
 //  Created by 양원식 on 4/28/25.
 //
 
+import Foundation
+
+final class FetchReservationsUseCase {
+    private let reservationRepository: ReservationRepository
+
+    init(reservationRepository: ReservationRepository) {
+        self.reservationRepository = reservationRepository
+    }
+
+    func execute(userId: String) async -> Result<[Reservation], Error> {
+        do {
+            let reservations = try await reservationRepository.fetchReservations(userID: userId)
+            return .success(reservations)
+        } catch {
+            return .failure(error)
+        }
+    }
+}
