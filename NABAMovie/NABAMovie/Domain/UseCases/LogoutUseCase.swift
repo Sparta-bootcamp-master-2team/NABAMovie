fd6d0b5eec1845b2479100f43c59e24b9a75fe4f
gnoes-ios <park_juseong@naver.com>
2025-04-30 13:32:18 +0900

    feat: 로그아웃 UseCase 구현

--- NABAMovie/NABAMovie/Domain/UseCases/LogoutUseCase.swift
+++ NABAMovie/NABAMovie/Domain/UseCases/LogoutUseCase.swift
@@ -5,3 +5,21 @@
 //  Created by 양원식 on 4/28/25.
 //
 
+import Foundation
+
+final class LogoutUseCase {
+    private let repository: AuthRepository
+    
+    init(repository: AuthRepository) {
+        self.repository = repository
+    }
+    
+    func execute() async -> Bool {
+        do {
+            try await repository.logout()
+            return true
+        } catch {
+            return false
+        }
+    }
+}
