d724c12a2d9904668441e58a0d2f4130b7ddc126
minjae <qlqlzbto@naver.com>
2025-04-30 14:17:59 +0900

    feat: ReservationDetailView 생성 / QRcode 이미지 에셋 추가

---
+++ NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ReservationDetailView.swift
+//  NABAMovie
+//
+//  Created by MJ Dev on 4/30/25.
+//
+
+import UIKit
+
+class ReservationDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
