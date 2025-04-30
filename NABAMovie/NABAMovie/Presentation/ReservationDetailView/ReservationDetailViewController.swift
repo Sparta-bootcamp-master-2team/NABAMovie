59775e6e19a4d7922e3d6acceb889ba2d3ff7ccd
minjae <qlqlzbto@naver.com>
2025-04-30 15:03:57 +0900

    feat: ReservationDetailVC전입 시 VM과 선택된 Reservation 데이터를 주입하도록 구현

--- NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
+++ NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
@@ -6,24 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ReservationDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let reservationDetailView = ReservationDetailView()
+    private let viewModel: ReservationDetailViewModel
+    private let reservationItem: Reservation
+    
+    init(viewModel: ReservationDetailViewModel, reservationItem: Reservation) {
+        self.viewModel = viewModel
+        self.reservationItem = reservationItem
+        reservationDetailView.configure(model: self.reservationItem)
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubviews([reservationDetailView])
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        reservationDetailView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
 }
