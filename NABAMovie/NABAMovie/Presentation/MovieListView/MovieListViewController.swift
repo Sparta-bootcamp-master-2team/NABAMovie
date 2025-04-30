b5614b6709c063963c55aa38e597947cba436b33
minjae <qlqlzbto@naver.com>
2025-05-01 02:59:20 +0900

    feat: MovieListViewModel 구현 및 MovieListVC에서 예매내역인 경우 예매내역 상세페이지로 전환, 찜목록인 경우 영화 상세페이지로 전환 하도록 구현

--- NABAMovie/NABAMovie/Presentation/MovieListView/MovieListViewController.swift
+++ NABAMovie/NABAMovie/Presentation/MovieListView/MovieListViewController.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import SnapKit
 
 class MovieListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private let collectionView = MovieItemCollectionView()
+    private let viewModel: MovieListViewModel
+    
+    init(viewModel: MovieListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        collectionView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubviews([collectionView])
+        configureLayout()
+        collectionView.fetchCellItems(item: viewModel.item)
+    }
+    
+    private func configureLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
 
+extension MovieListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let item = self.collectionView.movieItemCollectionDataSource?.itemIdentifier(for: indexPath) else { return }
+        switch item {
+        case .movieEntity(let movieEntity):
+            let movieInfoViewModel = MovieInfoViewModel(movieDetail: movieEntity)
+            let movieInfoVC = MovieInfoViewController(viewModel: movieInfoViewModel)
+            self.present(movieInfoVC, animated: true)
+        case .reservationEntity(let reservation):
+            let usecase = CancelReservationUseCase(repository: ReservationRepositoryImpl(firebaseService: FirebaseService()))
+            let reservationDetailViewModel = ReservationDetailViewModel(cancelReservationUseCase: usecase, reservationItem: reservation)
+            let reservationDetailVC = ReservationDetailViewController(viewModel: reservationDetailViewModel)
+            self.present(reservationDetailVC, animated: true)
+        }
+    }
 }
