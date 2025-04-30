05c5b0787140eff3c1031c79802ce92e1e87578b
minjae <qlqlzbto@naver.com>
2025-04-30 15:41:59 +0900

    feat: 뷰모델의 클로저 바인딩, 예매취소 버튼 클릭 시 alert를 통해 재확인을 사용자로부터 받고 예매 결과에 따른 alert 표시

--- NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
+++ NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class ReservationDetailViewController: UIViewController {
+final class ReservationDetailViewController: UIViewController {
 
     private let reservationDetailView = ReservationDetailView()
     private let viewModel: ReservationDetailViewModel
@@ -19,6 +19,8 @@ class ReservationDetailViewController: UIViewController {
         self.reservationItem = reservationItem
         reservationDetailView.configure(model: self.reservationItem)
         super.init(nibName: nil, bundle: nil)
+        bind()
+        self.reservationDetailView.delegate = self
     }
     
     required init?(coder: NSCoder) {
@@ -32,10 +34,27 @@ class ReservationDetailViewController: UIViewController {
         configureLayout()
     }
     
+    private func bind() {
+        viewModel.failedCancelReservation = { [unowned self] in
+            self.showAlert(title: "오류", message: "예매 취소 실패하였습니다.\n잠시후 다시 시도해주세요.")
+        }
+        viewModel.successCancelReservation = { [unowned self] in
+            self.showAlert(title: "예매 취소", message: "성공적으로 예매 취소되었습니다.")
+        }
+    }
+    
     private func configureLayout() {
         reservationDetailView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
+}
+
+extension ReservationDetailViewController: ReservationDetailViewDelegate {
+    func reservationCancelButtonTapped() {
+        self.showAlert(title: "취소 확인", message: "예매 취소하시겠습니까?", cancellable: true) { [unowned self] in
+            viewModel.cancelReservation(userID: "임시문자열 (수정에정)", reservationID: reservationItem.reservationID)
+        }
+    }
 }
--- NABAMovie/NABAMovie/Presentation/ReservationDetailView/ViewModel/ReservationDetailViewModel.swift
+++ NABAMovie/NABAMovie/Presentation/ReservationDetailView/ViewModel/ReservationDetailViewModel.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
-class ReservationDetailViewModel {
+final class ReservationDetailViewModel {
     
+    private let cancelReservationUseCase: CancelReservationUseCase
+    // 임시로 일단 불러옴
+    private let firebaseService = FirebaseService()
+    
+    init(cancelReservationUseCase: CancelReservationUseCase) {
+        self.cancelReservationUseCase = cancelReservationUseCase
+    }
+    
+    var failedCancelReservation: (() -> Void)?
+    var successCancelReservation: (() -> Void)?
+    
+    func cancelReservation(userID: String, reservationID: String) {
+        Task {
+            do {
+                let uid = try firebaseService.getCurrentUserId()
+                try await cancelReservationUseCase.execute(userID: uid, reservationID: reservationID)
+                await MainActor.run {
+                    successCancelReservation?()
+                }
+            } catch {
+                await MainActor.run {
+                    failedCancelReservation?()
+                }
+            }
+        }
+    }
 }
