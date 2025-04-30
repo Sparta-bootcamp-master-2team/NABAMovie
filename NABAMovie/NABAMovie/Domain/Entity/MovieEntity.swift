bc98bbe0db957439ca37a1836b917f3db4a206b0
_GGDol <bohg6799@naver.com>
2025-04-30 10:24:19 +0900

    chore: MovieEntitiy 수정

--- NABAMovie/NABAMovie/Domain/Entity/MovieEntity.swift
+++ NABAMovie/NABAMovie/Domain/Entity/MovieEntity.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-struct MovieEntity {
+struct MovieEntity: Hashable {
     let movieID: Int                // 영화 ID
     let title: String               // 영화 제목
     let genre: [String]             // 장르
-    let director: String?           // 영화 감독
+    let director: String            // 영화 감독
     let actors: [String]            // 출연 배우 (3명)
-    let releaseDate: String?        // 영화 개봉일
+    let releaseDate: String         // 영화 개봉일
     let runtime: Int                // 상영시간
     let voteAverage: Double         // 평점(소수점 두자리만 사용)
     let voteCount: Int              // 평가 인원 수
-    let overview: String?          // 줄거리
+    let overview: String            // 줄거리
     let posterImageURL: String      // 포스터 이미지 URL
-    let certification: String?      // 관람 등급
+    let certification: String       // 관람 등급
 }
