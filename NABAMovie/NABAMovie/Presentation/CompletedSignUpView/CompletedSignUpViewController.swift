b375cbaa609490549c39e814ee492e0a024ae40a
minjae <qlqlzbto@naver.com>
2025-05-01 09:47:06 +0900

    feat: CompletedSignUpVC 생성

---
+++ NABAMovie/NABAMovie/Presentation/CompletedSignUpView/CompletedSignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CompletedSignUpViewController.swift
+//  NABAMovie
+//
+//  Created by MJ Dev on 5/1/25.
+//
+
+import UIKit
+
+class CompletedSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
