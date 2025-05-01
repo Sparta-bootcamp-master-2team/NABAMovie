3fcf10905549e5f9031c60ef55ab1ebc62aad06e
minjae <qlqlzbto@naver.com>
2025-05-02 00:16:18 +0900

    refactor: 예매내역 상세페이지 다크모드 대응

--- NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
+++ NABAMovie/NABAMovie/Presentation/ReservationDetailView/ReservationDetailViewController.swift
@@ -27,7 +27,7 @@ final class ReservationDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         view.addSubviews([reservationDetailView])
         configureLayout()
     }
